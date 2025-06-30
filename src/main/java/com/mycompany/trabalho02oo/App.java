a8e8801d1c3f38f234eaf16465c3669307f630a6
AureaStar <aurea.starlight@gmail.com>
2025-06-30 14:37:54 -0300

    Configuração inicial com maven

---
+++ src/main/java/com/mycompany/trabalho02oo/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho02oo;
+
+/**
+ *
+ * @author 84398
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
